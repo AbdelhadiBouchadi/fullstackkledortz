51ef17c956ed287dee558f158dad7980fe64a296
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2024-09-29 14:25:21 +0300

    update

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -9,13 +9,13 @@ const ContactUs = () => {
     <div className="max-w-[80vw] w-full h-full flex flex-col justify-center items-center shadow-xl bg-white dark:bg-black py-4 md:py-8 rounded-xl overflow-hidden">
       <div className="w-full flex flex-col items-center justify-center px-20 gap-4 md:gap-8 lg:gap-12 py-4 md:py-8 lg:py-12">
         <h3 className="font-bold text-xl lg:text-3xl">Booking :</h3>
-        <h5 className="font-normal text-xl lg:text-3xl text-center">
+        <h5 className="font-normal text-xl lg:text-3xl text-center project-title">
           Sunset Agency - Adélia Cailleux
         </h5>
         <div className="flex flex-col lg:flex-row items-center justify-center gap-2 lg:gap-4">
           <Link
             href="https://wa.me/+33603211791"
-            className="underline text-lg text-center"
+            className="underline text-lg text-center project-title"
             target="_blank"
           >
             +33 6 03 2117 91
@@ -23,7 +23,7 @@ const ContactUs = () => {
           <span className="hidden lg:inline-block font-semibold">/</span>
           <Link
             href="mailto:adelia.booking@gmail.com?subject=Subject%20Here&body=Body%20Text%20Here"
-            className="underline text-lg text-center"
+            className="underline text-lg text-center project-title"
           >
             adelia.booking@gmail.com
           </Link>
@@ -32,7 +32,7 @@ const ContactUs = () => {
         <div className="flex flex-col lg:flex-row items-center justify-center gap-2 lg:gap-4">
           <Link
             href="https://wa.me/+33660587839"
-            className="underline text-lg text-center"
+            className="underline text-lg text-center project-title"
             target="_blank"
           >
             +33 6 60 58 78 39
@@ -40,7 +40,7 @@ const ContactUs = () => {
           <span className="hidden lg:inline-block font-semibold">/</span>
           <Link
             href="mailto:kevin.ledortz.mgt@gmail.com?subject=Subject%20Here&body=Body%20Text%20Here"
-            className="underline text-lg text-center"
+            className="underline text-lg text-center project-title"
           >
             kevin.ledortz.mgt@gmail.com
           </Link>
